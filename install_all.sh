799593e85792cc4943256f1bd0535acdd51b7184
Zahid <thezaza102@gmail.com>
2020-10-16 08:03:00 +1100

    changes

---
+++ install_all.sh
@@ -0,0 +1,12 @@
+conda install -y pandas
+conda install -y scikit-learn 
+conda install -y -c fvcore fvcore
+conda install -y -c conda-forge av 
+conda install -y -c anaconda scipy
+conda install -y -c anaconda opencv 
+conda install -y -c anaconda zeromq 
+conda install -y -c conda-forge yacs
+conda install -y -c conda-forge imutils
+conda install -y -c conda-forge simplejson
+conda install -y pytorch torchvision cudatoolkit=10.1 -c pytorch
+python -m pip install detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.6/index.html
